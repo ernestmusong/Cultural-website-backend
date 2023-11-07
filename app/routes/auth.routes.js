657b24b94adb22e374c0b50b5263f21720887f12
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-11-07 13:29:49 +0100

    Fix routes

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,4 +1,3 @@
-const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 
 module.exports = function(app) {
@@ -12,10 +11,6 @@ module.exports = function(app) {
 
   app.post(
     "/api/auth/signup",
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted
-    ],
     controller.signup
   );
 
